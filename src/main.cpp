78c54446a678cb7d2b8f59760b0ad5d5d5c5974c
Jesse Leventhal <45154268+jessel92@users.noreply.github.com>
2023-12-20 16:59:54 -0500

    trtied to make matrix func

--- src/main.cpp
+++ src/main.cpp
@@ -48,7 +48,7 @@ char keyMap[numRows][numCols] = {
 byte rowPins[numRows] = {1, 2, 3, 10, 11,6};    // Connect to the row pinouts of the button matrix
 byte colPins[numCols] = {14, 9, 8, 7};       // Connect to the column pinouts of the button matrix
 
-//char keyPressed;
+//char numPadKey;
 
 
 String input = "";  // String to store the user input
@@ -315,29 +315,29 @@ void keyPadFunc() {
       if (digitalRead(rowPins[row]) == LOW) {
        
         // Send keycode on keypress
-        char keyPressed = (keyMap[row][col]);
+        char numPadKey = (keyMap[row][col]);
 
-        if (keyPressed == 'p'){ Keyboard.printf("print");}
-        if (keyPressed == 'C'){ Keyboard.key_code_raw(KEY_BACKSPACE);}
-        if (keyPressed == 'n'){ Keyboard.key_code_raw(KEY_NUMLOCK);}
-        if (keyPressed == '*'){ Keyboard.key_code_raw(KEY_KPASTERISK);}
-        if (keyPressed == '/'){ Keyboard.key_code_raw(KEY_KPSLASH);}
-        if (keyPressed == '-'){ Keyboard.key_code_raw(KEY_KPMINUS);}
-        if (keyPressed == '+'){ Keyboard.key_code_raw(KEY_KPPLUS);}
-        if (keyPressed == '='){ Keyboard.key_code_raw(KEY_KPENTER);}
-        if (keyPressed == '.'){ Keyboard.key_code_raw(KEY_KPDOT);}
-        if (keyPressed == '0'){ Keyboard.key_code_raw(KEY_KP0);}
-        if (keyPressed == '1'){ Keyboard.key_code_raw(KEY_KP1);}
-        if (keyPressed == '2'){ Keyboard.key_code_raw(KEY_KP2);}
-        if (keyPressed == '3'){ Keyboard.key_code_raw(KEY_KP3);}
-        if (keyPressed == '4'){ Keyboard.key_code_raw(KEY_KP4);}
-        if (keyPressed == '5'){ Keyboard.key_code_raw(KEY_KP5);}
-        if (keyPressed == '6'){ Keyboard.key_code_raw(KEY_KP6);}
-        if (keyPressed == '7'){ Keyboard.key_code_raw(KEY_KP7);}
-        if (keyPressed == '8'){ Keyboard.key_code_raw(KEY_KP8);}
-        if (keyPressed == '9'){ Keyboard.key_code_raw(KEY_KP9);}
-        if (keyPressed == 'a'){ Keyboard.key_code_raw(KEY_ESC);}
-        if (keyPressed == 'b'){ Keyboard.key_code_raw(KEY_DELETE);}
+        if (numPadKey == 'p'){ Keyboard.printf("print");}
+        if (numPadKey == 'C'){ Keyboard.key_code_raw(KEY_BACKSPACE);}
+        if (numPadKey == 'n'){ Keyboard.key_code_raw(KEY_NUMLOCK);}
+        if (numPadKey == '*'){ Keyboard.key_code_raw(KEY_KPASTERISK);}
+        if (numPadKey == '/'){ Keyboard.key_code_raw(KEY_KPSLASH);}
+        if (numPadKey == '-'){ Keyboard.key_code_raw(KEY_KPMINUS);}
+        if (numPadKey == '+'){ Keyboard.key_code_raw(KEY_KPPLUS);}
+        if (numPadKey == '='){ Keyboard.key_code_raw(KEY_KPENTER);}
+        if (numPadKey == '.'){ Keyboard.key_code_raw(KEY_KPDOT);}
+        if (numPadKey == '0'){ Keyboard.key_code_raw(KEY_KP0);}
+        if (numPadKey == '1'){ Keyboard.key_code_raw(KEY_KP1);}
+        if (numPadKey == '2'){ Keyboard.key_code_raw(KEY_KP2);}
+        if (numPadKey == '3'){ Keyboard.key_code_raw(KEY_KP3);}
+        if (numPadKey == '4'){ Keyboard.key_code_raw(KEY_KP4);}
+        if (numPadKey == '5'){ Keyboard.key_code_raw(KEY_KP5);}
+        if (numPadKey == '6'){ Keyboard.key_code_raw(KEY_KP6);}
+        if (numPadKey == '7'){ Keyboard.key_code_raw(KEY_KP7);}
+        if (numPadKey == '8'){ Keyboard.key_code_raw(KEY_KP8);}
+        if (numPadKey == '9'){ Keyboard.key_code_raw(KEY_KP9);}
+        if (numPadKey == 'a'){ Keyboard.key_code_raw(KEY_ESC);}
+        if (numPadKey == 'b'){ Keyboard.key_code_raw(KEY_DELETE);}
         
         // Wait until the button is released
         while (digitalRead(rowPins[row]) == LOW) {
@@ -356,21 +356,19 @@ void keyPadFunc() {
 
 }
 
-/*
-char keyMatrixPressed(){
-  char key = 0;
-  for (byte col = 0; col < numCols; col++) {
-    digitalWrite(colPins[col], LOW);
-    for (byte row = 0; row < numRows; row++) {
-      if (digitalRead(rowPins[row]) == LOW) {
-        key = keyMap[row][col];
-        while (digitalRead(rowPins[row]) == LOW) {
-          // Do nothing
-        }
-      }
-    }
-    digitalWrite(colPins[col], HIGH);
-  }
-  return key;
-}
-*/
+// char keyMatrixPressed(){
+//   //char key = 0;
+//   for (byte col = 0; col < numCols; col++) {
+//     digitalWrite(colPins[col], LOW);
+//     for (byte row = 0; row < numRows; row++) {
+//       if (digitalRead(rowPins[row]) == LOW) {
+//         key = keyMap[row][col];
+//         while (digitalRead(rowPins[row]) == LOW) {
+//           // Do nothing
+//         }
+//       }
+//     }
+//     digitalWrite(colPins[col], HIGH);
+//   }
+//   return keyPressed;
+// }
