88e761834f9e2fc8106fd5f376aef69e1a0add3d
Jesse Leventhal <45154268+jessel92@users.noreply.github.com>
2023-12-20 20:02:53 -0500

    move matrix func to suji misc

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@ Firmware
 Copyright Jesse Leventhal
 ****************************************************/
 #include <Arduino.h>
+
 #include "header.h"
 #include "calc_func.h"
 #include "keypad_func.h"
@@ -120,26 +121,4 @@ void loop() {
   }
 }
 
-// Read Matrix Keypad ============================================================================================================
 
-// char keyMatrixPressed()
-// {
-//   char keyPressed = 0;
-//   for (byte col = 0; col < numCols; col++)
-//   {
-//     digitalWrite(colPins[col], LOW);
-//     for (byte row = 0; row < numRows; row++)
-//     {
-//       if (digitalRead(rowPins[row]) == LOW)
-//       {
-//         keyPressed = keyMap[row][col];
-//         while (digitalRead(rowPins[row]) == LOW)
-//         {
-//           // Do nothing
-//         }
-//       }
-//     }
-//     digitalWrite(colPins[col], HIGH);
-//   }
-//   return keyPressed;
-// }
--- src/suji_misc_funcs.h
+++ src/suji_misc_funcs.h
@@ -1,2 +1,25 @@
 #include "header.h"
 
+// Read Matrix Keypad ============================================================================================================
+
+char keyMatrixPressed()
+{
+  char keyPressed = 0;
+  for (byte col = 0; col < numCols; col++)
+  {
+    digitalWrite(colPins[col], LOW);
+    for (byte row = 0; row < numRows; row++)
+    {
+      if (digitalRead(rowPins[row]) == LOW)
+      {
+        keyPressed = keyMap[row][col];
+        while (digitalRead(rowPins[row]) == LOW)
+        {
+          // Do nothing
+        }
+      }
+    }
+    digitalWrite(colPins[col], HIGH);
+  }
+  return keyPressed;
+}
